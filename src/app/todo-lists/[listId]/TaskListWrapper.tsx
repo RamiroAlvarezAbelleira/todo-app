005c360f3b12f977f7abff18b400aab8e2e257d3
Ramiro Alvarez Abelleira <alvarezramiro1997@gmail.com>
2025-08-04 13:52:29 +0200

    updated taskListWrapper

--- src/app/todo-lists/[listId]/TaskListWrapper.tsx
+++ src/app/todo-lists/[listId]/TaskListWrapper.tsx
@@ -52,18 +52,20 @@ const TaskListWrapper = ({ todo_list_id, title }: TaskListWrapperProps) => {
                 </button>
                 <h1 className="font-bold text-2xl">{title}</h1>
             </div>
-            <div className="flex flex-col items-start bg-gray-100 rounded-xl flex-1 overflow-y-auto scroll-container px-1.5 py-2">
+            <div className="flex flex-col items-start justify-start bg-gray-100 rounded-xl flex-1 overflow-y-auto scroll-container px-1.5 py-2">
                 <TaskList
                     tasks={tasks}
                     setTasks={setTasks}
                 />
                 {
                     showCreate ?
-                        <TaskForm
-                            onSubmit={createTaskFunc}
-                            buttonLabel="Add"
-                            setShowState={setShowCreate}
-                        />
+                        <div className="py-2">
+                            <TaskForm
+                                onSubmit={createTaskFunc}
+                                buttonLabel="Add"
+                                setShowState={setShowCreate}
+                            />
+                        </div>
                         :
                         <AddButton setShowState={setShowCreate} />
                 }
